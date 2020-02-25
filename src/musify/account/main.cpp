49fbb97d305aa9f44458bfec9403f70faa4924e0
Pascal Menuet <menuet@users.noreply.github.com>
2020-02-25 12:09:49 +0100

    musify account 1 : functions, parameters, namespaces, const, auto
    Functions (https://en.cppreference.com/w/cpp/language/functions)
    Namespaces (https://en.cppreference.com/w/cpp/keyword/namespace)
    Namespaces aliases (https://en.cppreference.com/w/cpp/language/namespace_alias)
    Fix UB:
    Value-initialization with curly braces (https://en.cppreference.com/w/cpp/language/value_initialization)
    const (https://en.cppreference.com/w/cpp/language/cv)
    auto (https://en.cppreference.com/w/cpp/language/auto)

--- src/musify/account/main.cpp
+++ src/musify/account/main.cpp
@@ -2,29 +2,57 @@
 #include <iostream>
 #include <string>
 
+namespace musify { namespace io {
+
+    std::string ask_question_get_string(std::string question)
+    {
+        std::cout << question << " ? ";
+        std::string value{};
+        std::cin >> value;
+        return value;
+    }
+
+    char ask_question_get_char(std::string question)
+    {
+        std::cout << question << " ? ";
+        char value{};
+        std::cin >> value;
+        return value;
+    }
+
+    int ask_question_get_int(std::string question)
+    {
+        std::cout << question << " ? ";
+        int value{};
+        std::cin >> value;
+        return value;
+    }
+
+    double ask_question_get_double(std::string question)
+    {
+        std::cout << question << " ? ";
+        double value{};
+        std::cin >> value;
+        return value;
+    }
+
+}} // namespace musify::io
+
 int main()
 {
+    namespace mio = musify::io;
+
     std::cout << "Please create your account.\n";
 
-    std::cout << "What is your name ? ";
-    std::string name;
-    std::cin >> name;
+    const auto name = mio::ask_question_get_string("What is your name");
 
-    std::cout << "What is your password ? ";
-    std::string password;
-    std::cin >> password;
+    const auto password = mio::ask_question_get_string("What is your password");
 
-    std::cout << "What is your gender (M/F) ? ";
-    char gender;
-    std::cin >> gender;
+    const auto gender = mio::ask_question_get_char("What is your gender (M/F)");
 
-    std::cout << "What is your age ? ";
-    int age;
-    std::cin >> age;
+    const auto age = mio::ask_question_get_int("What is your age (years)");
 
-    std::cout << "What is your size ? ";
-    double size;
-    std::cin >> age;
+    const auto size = mio::ask_question_get_double("What is your size (meters)");
 
     std::cout << "Thanks. Your account was successfully created:\n"
               << "Name: " << name << "\n"
